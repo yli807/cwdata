6a0322f6b3422f2f90240b72cd412782ab715998
Yanhui LI <yli807@student.monash.edu>
2021-10-18 03:31:53 +1100

    update data documentation

--- R/key_crop_yields.R
+++ R/key_crop_yields.R
@@ -1,4 +1,18 @@
+#' Title of the data
+#'
+#' More details about the data
+#'
+#' @section Background:
+#' The data was collected from ... over the course of ....
+#'
+#' @format A data frame with 143,825 rows and 5 variables:
+#' \describe{
+#'   \item{entity}{country names, around the world}
+#'   }
+#'
+#' @source
+#'
+#' @examples
+#' print(key_crop_yields)
+
 "key_crop_yields"
-mean <- function(x){
-  sum(x)/length(x)
-}
